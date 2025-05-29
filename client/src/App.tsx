66564aa6e8276dfee9f2faff9f6a1e4fd3317734
arthurazevedods <arthurazevedods@gmail.com>
2025-05-28 21:16:07 -0300

    footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
-
 import "./App.css";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <>
-    <Navbar/>
-  </>;
+  return (
+    <div className="flex flex-col min-h-[100vh]">
+      <Navbar />
+      {/* Conteúdo principal */}
+      <main className="flex-1">
+        {/* ... */}
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
