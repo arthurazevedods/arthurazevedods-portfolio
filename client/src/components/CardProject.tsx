7635cc66d7dae7c487ded61e2450bdd7d2843f12
arthurazevedods <arthurazevedods@gmail.com>
2025-06-09 19:22:04 -0300

    card project & theme fixed on comp

--- client/src/components/CardProject.tsx
+++ client/src/components/CardProject.tsx
@@ -1,23 +1,26 @@
+import LinkIcon from '@mui/icons-material/Link';
 
-function CardProject(){
-    return(
-        <div className="rounded flex justify-around bg-white">
-            <div>
-                <div>
-                <h4 className="text-black">Valfenda</h4>
-                <span className="text-black">something</span>
-                </div>
-
-                <div className="text-black">Uma aplicação de biblioteca pessoal, criada em react e golang.</div>
-            </div>
-            {/*Buttons */}
-            <div>
-                <button className="rounded-full w-10 h-10 border-1 border-b-black text-black">
-                    9
-                </button>
-            </div>
+function CardProject() {
+  return (
+    <div className="rounded-[20px] flex justify-around bg-[var(--card)] p-4 gap-1">
+      <div className="flex flex-col text-start">
+        <div className="pb-5">
+          <h4 className="text-[var(--foreground)]">Valfenda</h4>
+          <span className="text-[var(--color-secondary)]">something</span>
         </div>
-    )
+
+        <div className="text-[var(--foreground)]">
+          Uma aplicação de biblioteca pessoal, criada em react e golang.
+        </div>
+      </div>
+      {/*Buttons */}
+      <div className="flex flex-col justify-end">
+        <button className="rounded-full w-10 h-10 border border-[var(--border)] text-[var(--foreground)]">
+          <LinkIcon />
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default CardProject
+export default CardProject;
--- client/src/components/RecentWorks.tsx
+++ client/src/components/RecentWorks.tsx
@@ -4,7 +4,7 @@ function RecentWorks(){
     return(
         <section>
             <h3 className="flex justify-center text-4xl bold pb-8 ">Trabalhos Recentes</h3>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mx-4">
                 <CardProject />
                 <CardProject />
                 <CardProject />
