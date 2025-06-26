343c7cb133e614e27b0f1c746accbf21b5675e69
arthurazevedods <arthurazevedods@gmail.com>
2025-06-25 23:50:33 -0300

    rm links from root

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,20 +1,24 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      {/** 
       <div className="p-2 flex gap-2">
+        
         <Link to="/" className="[&.active]:font-bold">
           Home
         </Link>{' '}
         <Link to="/projects" className="[&.active]:font-bold">
           Projetos
         </Link>
+        
       </div>
+      */}
       <hr />
       <Outlet />
       <TanStackRouterDevtools />
     </>
   ),
-})
+});
